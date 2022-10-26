3df06033892d4d73d330ca69da152cd653106790
cwide <codytenney@gmail.com>
2022-10-26 14:42:20 -0500

    refactor: use query param builder

--- wmi.go
+++ wmi.go
@@ -3,6 +3,7 @@ package nhtsa
 import (
 	"errors"
 	"fmt"
+	"net/url"
 )
 
 func GetWmiForManufacturer(mId string, vType string) (*BaseApiResponse[GetWmiResult], error) {
@@ -10,15 +11,16 @@ func GetWmiForManufacturer(mId string, vType string) (*BaseApiResponse[GetWmiRes
 		return nil, errors.New("you must provide a manufacturer id or vehicle type")
 	}
 
-	reqUrl := fmt.Sprintf("%s/GetWMIsForManufacturer/%s", nhtsaBaseUrl, mId)
+	reqUrl := fmt.Sprintf("%s/GetWMIsForManufacturer/%s?", nhtsaBaseUrl, mId)
+	queryParams := url.Values{}
 
-	if vType == "" {
-		reqUrl += "?format=json"
-	} else {
-		reqUrl += fmt.Sprintf("?vehicleType=%s&format=json", vType)
+	if vType != "" {
+		queryParams.Add("vehicleType", vType)
 	}
 
-	res, err := doRequest[BaseApiResponse[GetWmiResult]](reqUrl)
+	queryParams.Add("format", "json")
+
+	res, err := doRequest[BaseApiResponse[GetWmiResult]](reqUrl + queryParams.Encode())
 	if err != nil {
 		return nil, fmt.Errorf("error getting WMI: %s", err)
 	}
