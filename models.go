d4e7290d180eba83ac82e8af38906527c3b2508e
cwide <codytenney@gmail.com>
2022-10-27 15:40:48 -0500

    feat: models methods init

--- models.go
+++ models.go
@@ -2,8 +2,19 @@ package nhtsa
 
 import "fmt"
 
-func GetModelsForMake(mName string) (*BaseApiResponse[GetModelsResult], error) {
+func GetModelsForMakeByName(mName string) (*BaseApiResponse[GetModelsResult], error) {
 	reqUrl := fmt.Sprintf("%s/GetModelsForMake/%s?format=json", nhtsaBaseUrl, mName)
+
+	res, err := doRequest[BaseApiResponse[GetModelsResult]](reqUrl)
+	if err != nil {
+		return nil, fmt.Errorf("error getting models: %s", err)
+	}
+
+	return res, nil
+}
+
+func GetModelsForMakeById(mId string) (*BaseApiResponse[GetModelsResult], error) {
+	reqUrl := fmt.Sprintf("%s/GetModelsForMakeId/%s?format=json", nhtsaBaseUrl, mId)
 
 	res, err := doRequest[BaseApiResponse[GetModelsResult]](reqUrl)
 	if err != nil {
